932a79aac58e48c11bbcad7ea9ddca7e4fddab68
bketkar <bketkar@gmail.com>
2016-03-13 17:26:00 +0530

    Added files via upload

--- Plot4.R
+++ Plot4.R
@@ -8,6 +8,7 @@ plot(plotdata$Date,plotdata$Voltage,type="l",xlab="Datetime",ylab="Voltage")
 plot(plotdata$Date,plotdata$Sub_metering_1,col="black",type="l",xlab="",ylab="Energy sub metering")
 lines(plotdata$Date,plotdata$Sub_metering_2,col="red",type="l")
 lines(plotdata$Date,plotdata$Sub_metering_3,col="blue",type="l")
+legend("topright",legend=c("submetering1","submetering2","submetering3"),col=c("black","red","blue"),lty=c(1,1,1))
 
 plot(plotdata$Date,plotdata$Global_reactive_power,type="l",xlab="Datetime",ylab="Global reactive power")
 
